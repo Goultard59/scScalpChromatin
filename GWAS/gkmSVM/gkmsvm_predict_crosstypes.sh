c870b890be2974e8949cb2b82bf0c38133e977e5
Goultard59 <goultard59@gmail.com>
2023-09-01 15:39:57 +0200

    Update gkmsvm_predict_crosstypes.sh

--- GWAS/gkmSVM/gkmsvm_predict_crosstypes.sh
+++ GWAS/gkmSVM/gkmsvm_predict_crosstypes.sh
@@ -4,8 +4,11 @@
 #set -o errexit 
 
 ############################################
-# Fit each accessibility model on sequences from all cell types
+# Run gkmexplain on ref and alt SNPs 
 ############################################
+
+# https://github.com/kundajelab/lsgkm-svr
+# https://github.com/kundajelab/gkmexplain
 
 # Load required modules
 ml system gcc
@@ -14,113 +17,48 @@ ml system gcc
 export PATH=$HOME/work/lsgkm-svr/bin:$PATH
 
 # Directory containing fit models
-model_dir=/home/adufour/work/gskm/fit_models_1000bp
+model_dir=/home/adufour/work/gskm/full_models_1000bp
 
 # Directory for prediction results
-pred_dir=/home/adufour/work/gskm/model_predictions
-if [ ! -d ${pred_dir} ]; then mkdir ${pred_dir}; fi
+result_dir=/home/adufour/work/gskm/snp_explanations
+if [ ! -d ${result_dir} ]; then mkdir ${result_dir}; fi
 
 # Fasta files to predict
 fasta_dir=/home/adufour/work/gskm/fastas_1000bp_randOnly
-true_seq_files=(${fasta_dir}/*_true_seqs.fasta)
+snp_fastas=(${fasta_dir}/*snp_seqs*.fasta)
 
-# Cell type headers for models
-model_files=(${model_dir}/*.fold0.model.txt)
+# All previously fit models
+model_files=(${model_dir}/*.model.txt)
 file_basenames=("${model_files[@]##*/}")
-model_headers=("${file_basenames[@]/.fold0.model.txt/}")
+model_headers=("${file_basenames[@]/.model.txt/}")
 
-# Define training/testing folds
-all_chrs=($(seq 1 1 18))
-all_chrs+=("X")
-#all_chrs=("${all_chrs[@]/#/chr}")
-
-# 10-fold cross validation
-fold0=(1)
-fold1=(2 18)
-fold2=(3 13)
-fold3=(6 X)
-fold4=(5 16)
-fold5=(4 15)
-fold6=(7 14)
-fold7=(11 17)
-fold8=(9 12)
-fold9=(8 10)
-fold10=(Y)
-fold11=(AEMK02000133.1 AEMK02000137.1 AEMK02000146.1)
-
-#n_folds=($(seq 0 1 9))
-
-n_folds=(0) # Only use one fold for testing cross-types
-all_folds=("${n_folds[@]/#/fold}")
-
-# Directory to hold testing data
-test_dir=${fasta_dir}/testing_fastas
-if [ ! -d ${test_dir} ]; then mkdir ${test_dir}; fi
-
-########################################################################################
-# Predict testing data for each fold with each cell type
-########################################################################################
-
-job_header=gkm_test
+#############################################################
+# Predict accessibility for all SNP fastas using all models
+#############################################################
 
 for model in "${model_headers[@]}"
 do
-    for fold in "${all_folds[@]}"
+    # Prepare filepaths
+    model_file=${model_dir}/${model}.model.txt
+    job_header=gkm_250exp
+    outpre=${result_dir}/${model}
+
+    for fasta in "${snp_fastas[@]}"
     do
-        # Define fold chromosomes
-        fchr=${fold}[@]
-        test_chr=("${!fchr}")
-
-        echo "Testing chromosomes for ${fold}..."
-
-        # Model:
-        model_file=${model_dir}/${model}.${fold}.model.txt
-
-        # Output prefix
-        outpre=${pred_dir}/${model}.${fold}
-
-        for celltype in "${model_headers[@]}"
-        do
-            # Get true and null seq fasta files for testing
-            true_seq_files=( $(printf "${fasta_dir}/${celltype}_%s_true_seqs.fasta " "${test_chr[@]}") )
-            null_seq_files=( $(printf "${fasta_dir}/${celltype}_%s_null_seqs.fasta " "${test_chr[@]}") )
-
-            true_result=${outpre}.pred.${celltype}.true.txt
-            null_result=${outpre}.pred.${celltype}.null.txt
-
-            # First true seqs:
-            if [ ! -f ${true_result} ]
-            then
-                # Create files for combined testing data
-                true_seqs=${test_dir}/${celltype}.${fold}.true_seqs.fasta
-                if [ ! -f ${true_seqs} ]; then cat "${true_seq_files[@]}" > ${true_seqs}; fi
-
-                # Predict accessibility
-                true_log=${outpre}.pred.${celltype}.true.log
-                echo "Predicting true ${celltype} accessibility using ${model}..."
-                sbatch -t 12:00:00 --mem=10G --cpus-per-task=4 \
-                --job-name=${job_header} --output=${true_log} --error=${true_log} \
-                --wrap "gkmpredict -T 4 ${true_seqs} ${model_file} ${true_result}"
-            else
-                echo "File ${true_result} already exists! Skipping..."
-            fi
-            # Now null seqs:
-            if [ ! -f ${null_result} ]
-            then
-                null_seqs=${test_dir}/${celltype}.${fold}.null_seqs.fasta
-                if [ ! -f ${null_seqs} ]; then cat "${null_seq_files[@]}" > ${null_seqs}; fi
-
-                # Predict accessibility
-                null_log=${outpre}.pred.${celltype}.null.log
-                echo "Predicting null ${celltype} accessibility using ${model}..."
-                sbatch -t 12:00:00 --mem=10G --cpus-per-task=4 \
-                --job-name=${job_header} --output=${null_log} --error=${null_log} \
-                --wrap "gkmpredict -T 4 ${null_seqs} ${model_file} ${null_result}"
-            else
-                echo "File ${null_result} already exists! Skipping..."
-            fi
-        done
+        # Prepare output file
+        fasta_basename="${fasta##*/}"
+        fasta_basename="${fasta_basename/.fasta/}"
+        result_file=${outpre}.${fasta_basename}.gkmexplain.txt
+        log_file=${outpre}.${fasta_basename}.log
+        if [ ! -f ${result_file} ]
+        then
+            # Explain model importance
+            echo "Using model ${model} to calculate gkmexplain scores for ${fasta_basename}..."
+            sbatch --mem=12G --cpus-per-task=1 \
+            --job-name=${job_header} --output=${log_file} --error=${log_file} \
+            --wrap "gkmexplain ${fasta} ${model_file} ${result_file}"
+        else
+            echo "File ${result_file} already exists! Skipping..."
+        fi
     done
 done
-
-########################################################################################
